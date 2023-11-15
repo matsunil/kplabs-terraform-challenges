eec2f2c376cdb7181996b201d38c7f93833d6a16
Sunil Thomas Mathew <smathew@hbs.edu>
2023-11-15 10:11:30 -0500

    solved

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,15 +1,23 @@
 provider "aws" {
- version = "~> 2.54"
+#  version = "~> 2.54"
   profile = "terraform"
 }
 
-provider "digitalocean" {}
+# provider "digitalocean" {}
 
 terraform {
-  required_version = "0.12.31"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+    }
+  }
 }
 
+# terraform {
+#   required_version = "0.12.31"
+# }
 
 resource "aws_eip" "kplabs_app_ip" {
-  vpc = true
+  # vpc = true
+  domain = vpc
 }
